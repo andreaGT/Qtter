4e350db219d82a944c6ad28f06a59ec2e6e510ed
Andrea Alvarez <dry.lez@gmail.com>
2019-03-27 17:20:00 -0600

    Initial commit

---
+++ models/tuits.js
@@ -0,0 +1,80 @@
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+
+//var dPort = 27017;
+var dPort = 27020;
+//var dHost = "localhost";
+var dHost = "192.168.1.4";
+var dName = "tuiter";
+
+var tuiterDb = {};
+
+tuiterDb.db = new Db(dName, new Server(dHost, dPort,{auto_reconnect: true},{}))
+tuiterDb.db.open(function(e, d){
+	if(e) 
+		console.log(e)
+	else
+		console.log("Conectado a la base de datos: " + dName);
+});
+
+tuiterDb.tuits = tuiterDb.db.collection('tuits');
+module.exports = tuiterDb;
+
+//nuevo tuitit
+tuiterDb.new = function(newData, callback){
+	tuiterDb.tuits.insert(newData, callback(null))
+}
+
+tuiterDb.list = function(callback){
+	tuiterDb.tuits.find().sort({'time':-1}).toArray(function(e, res){
+		if(e)
+			callback(e)
+		else
+			callback(null, res)
+	})
+}
+
+tuiterDb.countTuits = function(callback){
+	tuiterDb.tuits.count(function(err,res){
+		if(err)
+			callback(err)
+		else
+			callback(null, res)
+	})
+}
+
+tuiterDb.userList = function(callback){
+	tuiterDb.tuits.distinct('user',function(err,items){
+		if(err)
+			callback(err)
+		else
+			callback(null,items)
+	})
+}
+
+tuiterDb.catList = function(callback){
+	tuiterDb.tuits.distinct('category',function(err,items){
+		if(err)
+			callback(err)
+		else
+			callback(null,items)
+	})
+}
+
+tuiterDb.userTuitList= function(usr,callback){
+	tuiterDb.tuits.find({'user':usr}).toArray(function(err, tuits){
+		if(err)
+			callback(err)
+		else
+			callback(null,tuits)
+	})
+}
+
+tuiterDb.catTuitList= function(cat, callback){
+	tuiterDb.tuits.find({'category':cat}).toArray(function(err, tuits){
+		if(err)
+			callback(err)
+		else
+			callback(null,tuits)
+	})
+}
---
+++ tuiter/models/users.js
@@ -0,0 +1,33 @@
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
+
+var dPort = 27017;
+var dHost = "localhost";
+var dName = "tuiter";
+
+var tuiterDb = {};
+
+tuiterDb.db = new Db(dName, new Server(dHost, dPort,{auto_reconnect: true},{}))
+tuiterDb.db.open(function(e, d){
+	if(e) 
+		console.log(e)
+	else
+		console.log("Conectado a la base de datos: " + dName);
+});
+
+tuiterDb.users = tuiterDb.db.collection('tuits');
+module.exports = tuiterDb;
+
+//nuevo usuario
+tuiterDb.new = function(newData, callback){
+	tuiterDb.users.insert(newData, callback(null))
+}
+
+tuiterDb.list = function(callback){
+	tuiterDb.users.find().toArray(function(e, res){
+		if(e)
+			callback(e)
+		else
+			callback(null, res)
+	})
+}
